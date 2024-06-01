3663ab1f236f8624e81d7a89041c59856c3a8763
Carlos Daniel C <carlosdanielcostadesousa02@gmail.com>
2024-06-01 17:14:17 -0300

    criadas as funcoes principais e comecando a emplementar-las

--- js/game.js
+++ js/game.js
@@ -5,9 +5,30 @@ class Game  {
         }
     start(maxRange){
         this.secretNumber = Math.floor(Math.random()*maxRange+1);
+        console.log(this.secretNumber)
     }
-    update(inputTarget){
-        
+    checkNumber(inputTarget){
+        if (inputTarget.value == ""){
+            alert("insira um numero valido antes de continuar")
+        }
+        else{
+            let userInput = parseInt(inputTarget.value);
+            if (userInput < 0 || userInput > 10){
+                alert("insira um numero entre 0 e 10")
+            }
+            if(userInput == this.secretNumber){
+                this.win(); 
+            }
+        }
+    }
+    win(){
+     alert("voce venceu.");   
+    }
+    gameOver(){
+        window.alert("você perdeu.");
+    }
+    restartGame(){
+        window.location.reload();
     }
 }
 
